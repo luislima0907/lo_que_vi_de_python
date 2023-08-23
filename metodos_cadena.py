dcf8c401524efabe98cc096f8510e13d415650fe
Luis <llimap5@miumg.edu.gt>
2023-08-22 19:04:01 -0600

    Este es mi segundo punto de control

--- metodos_cadena.py
+++ metodos_cadena.py
@@ -52,10 +52,86 @@ booleano_uno = True
 #atributos de un boleano
 #['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'as_integer_ratio', 'bit_count', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
 
-#Los métodos en python, primero va el nombre o el valor del dato, luego un punto ( . ) y de último el nombre del método seguido de un paréntesis ()
+#Los métodos en python, primero va el nombre o el valor del dato, luego un punto ( . ) y de último el nombre del método seguido de un paréntesis () que puede contener o no un parametro.
 
 #ejemplo
 texto_uno = "Que onda vos"
+texto_dos = "kIONDA"
+texto_tres = "hola como estas"
+
 #método upper, convierte cada letra de un texto o string en MAYUSCULAS
 prueba_de_metodo = texto_uno.upper()
 print(prueba_de_metodo)
+
+#método lower, convierte cada letra de un texto o string en minúsculas
+prueba_de_metodo_dos = texto_dos.lower()
+print(prueba_de_metodo_dos)
+
+#método capitalize, convierte la primera letra de un texto o string en inicial mayúscula
+prueba_de_metodo_tres = texto_tres.capitalize()
+print(prueba_de_metodo_tres)
+
+#método find, este encuentra la posición de un elemento dentro de un objeto, puede ser una string, lista, etc.
+#para especificar que valor queremos encontrar, debemos de darle el parámetro dentro de los paréntesis.
+#cuando este método no encuentra la posición de un elemento, nos dará como resultado -1 
+prueba_de_metodo_cuatro = texto_uno.find("s")
+print(prueba_de_metodo_cuatro)
+
+#metodo index, funciona casi igual al find solo que este cuando no encuentra un elemento da error, porque funciona como manejo de excepciones.
+
+#Esto da error y el programa se detiene.
+#prueba_de_metodo_cinco = texto_dos.index("5")
+#print(prueba_de_metodo_cinco)
+
+#método isnumeric, esta función encuentra un elemento númerico dentro de una string y dará como resultado True si lo llegará a encontrar.
+#cabe recordar que no le podemos dar un parametro, ya sea un número o cáracter, ya que esta función no está buscando el índice o posición del número.
+#sino mas bien, solo nos indicará si toda la string tiene elementos númerico con true o false.
+string_numerica = "8923140asdfjag"
+prueba_de_funcion = string_numerica.isnumeric()
+print(prueba_de_funcion)
+
+#método isalpha, esta función nos dará como resultado verdadero si encuentra una string que no tenga espacios o carácteres especiales.
+#como lo pueden ser los espacios o símbolos, etc, también no tienen que haber números dentro de la string, va a ser verdadera si la string solo es de las letras del abecedario A-Z.
+string_de_solo_letras = "aksdjfjqwfaskdfhoqv"
+prueba_de_funcion_dos = string_de_solo_letras.isalpha()
+print(prueba_de_funcion_dos)
+
+#método count, esta función cuenta las veces que se repite un elemento dentro de una string. Debemos de definir un parámetro para determinar el número de coincidencias.
+#si no encuentra ninguna repetición o coincidencia, se mostrará un 0.
+string_de_coincidencias = "hola como estas amigo"
+prueba_de_funcion_tres = string_de_coincidencias.count("o")
+print(prueba_de_funcion_tres)
+
+#método len, esta función muestra la cantidad de caráteres de una string.
+
+string_de_caracteres = "Hola, mundo."
+prueba_de_metodo_cinco = string_de_caracteres.__len__()
+print(prueba_de_metodo_cinco)
+
+
+#método startswith, este método nos dará como resultado verdadero si dentro del parámetro le colocamos las iniciales de nuestra string.
+
+string_de_iniciales = "Hola wachin kionda"
+prueba_de_metodo_seis = string_de_iniciales.startswith("Hol")
+print(prueba_de_metodo_seis)
+
+#método endswith, este método es similar al anterior, solo que este evalúa los caráteres que conformen el final de nuestra string.
+
+string_de_finales = "Quiero aprender word"
+prueba_de_metodo_siete = string_de_finales.endswith("rd")
+print(prueba_de_metodo_siete)
+
+#método replace, este método remplaza carácteres de nuestra string. Primero debemos de darle el primer parámetro o parte que queremos modificar en nuestra string en comillas y luego lo separamos por una coma.
+#una vez hecho eso, ya podemos definir el remplazo dentro de comillas.
+#cabe recordar que primero debemos de escribir bien la coincidencia o repeticion de nuestra string vieja para poder asignarle un nuevo valor despues, en caso de que no lo cumplamos, nos devolverá la string original.
+
+string_reemplazable = "Que buena onda" 
+string_nueva = string_reemplazable.replace("Que", "Gracias")
+print(string_nueva)
+
+#método split, este método separa las strings por medio del valor que le ingresemos en el parámetro.
+#el valor que pongamos dentro del parámetro se remplazará por una coma
+
+string_separable = "Yutu morado"
+prueba_de_metodo_ocho = string_separable.split(" ")
+print(prueba_de_metodo_ocho)
